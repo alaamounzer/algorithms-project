2435ce783f0280d6ab5d33848430dcf28007d8e2
alaamounzer <alaamounzer963@gmail.com>
2025-10-05 19:55:29 +0300

    find maximum in a list

--- algorithms.py
+++ algorithms.py
@@ -7,3 +7,15 @@ def factorial(n):
     return result
 
 print(factorial(5))  
+
+
+def find_max(numbers):
+    max = numbers[0] 
+    for num in numbers[1:]:  
+        if num > max:
+            max = num
+    return max
+
+print(find_max([3, 7, 2, 9, 5]))
+
+  
